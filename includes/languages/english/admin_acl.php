5eba0e42cdccd9bf28c66c2104b5beb16a9acbf7
Suresh Katiki <sureshkatiki007@users.noreply.github.com>
2015-05-01 16:19:17 +0530

    Create admin_acl.php

---
+++ includes/languages/english/admin_acl.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * admin_acl.php
+ * September 26, 2006
+ * author: Suresh.Katiki
+ * admin_acl version 1.0
+ *
+ *
+ * Released under the GNU General Public License
+ *
+ */
+
+define('HEADING_TITLE', 'Access Controls for Admin User %s');
+define('HEADING_AVAILABLE', 'Options Available');
+define('HEADING_SELECTED', 'Options Blocked');
+
+define('MESSAGE_ADMIN_ACL_SAVED', 'New access control rules saved.');
+
+define('ERROR_ADMIN_ACL_SAVE', 'Error saving new access rules.');
+
+?>
